a96a0ad4a6e4d6c3a454f95174deaf1498081a6a
Dylan <dylanyeowf@gmail.com>
2025-06-26 20:47:44 +0800

    updated routes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const aws = require("aws-sdk");
+const crypto = require('crypto');
 const dynamo = new aws.DynamoDB.DocumentClient();
 const cognito = new aws.CognitoIdentityServiceProvider();
 
@@ -52,6 +53,128 @@ const decodeJWT = (token, callback) => {
             body: JSON.stringify({ error: `Failed to decode JWT: ${e.message}` })
         });
         return null;
+    }
+}
+
+/**
+ * Calculates the secret hash to be passed into AWS Cognito
+ * 
+ * @param {*} username - The username
+ * @returns 
+ */
+function calculateSecretHash(username) {
+    const encoder = new TextEncoder();
+    const data = `${username}2lave0d420lofl9ead9h87mi41`;
+    const keyData = encoder.encode('kr3a1i8868lhcmmlain7jh10trjofrt4f2f4en2orh6oorbkp3t');
+    const dataToSign = encoder.encode(data);
+
+    return crypto.subtle.importKey(
+        "raw",
+        keyData,
+        { name: "HMAC", hash: { name: "SHA-256" } },
+        false,
+        ["sign"]
+    )
+        .then(function (key) {
+            return crypto.subtle.sign("HMAC", key, dataToSign)
+                .then(function (signature) {
+                    // Convert signature to base64 format
+                    const byteArray = new Uint8Array(signature);
+                    return btoa(String.fromCharCode.apply(null, byteArray));
+                });
+        });
+}
+
+/**
+ * Handles the new password challenge
+ * @param {*} session 
+ * @param {*} username 
+ * @param {*} newPassword 
+ * @param {*} callback
+ */
+function handleNewPasswordChallenge(session, username, newPassword, callback) {
+    calculateSecretHash(username)
+        .then(secretHash => {
+            const params = {
+                ChallengeName: 'NEW_PASSWORD_REQUIRED',
+                ClientId: '2lave0d420lofl9ead9h87mi41',  // Your App Client ID
+                ChallengeResponses: {
+                    USERNAME: username,
+                    NEW_PASSWORD: newPassword,
+                    SECRET_HASH: secretHash
+                },
+                Session: session,
+            };
+
+            cognito.respondToAuthChallenge(params, function (err, data) {
+                if (err) throw new Error("Error responding to password change challenge:", err);
+
+                if (data.AuthenticationResult) {
+                    callback(null, {
+                        statusCode: 200,
+                        body: JSON.stringify(data.AuthenticationResult),
+                    });
+                }
+            });
+        })
+        .catch(err => {
+            callback(null, {
+                statusCode: 500,
+                body: JSON.stringify(err),
+            });
+        });
+}
+
+/**
+ * Function to trigger the forget password flow
+ * @param {*} username 
+ */
+async function forgotPassword(username, callback) {
+    const secretHash = await calculateSecretHash(username);
+    const params = {
+        ClientId: '2lave0d420lofl9ead9h87mi41',
+        SecretHash: secretHash,
+        Username: username,
+    };
+
+    try {
+        await cognito.forgotPassword(params).promise();
+        callback(null, {
+            statusCode: 200,
+            body: JSON.stringify({ message: "Password reset code sent successfully" })
+        });
+    } catch (err) {
+        callback(null, {
+            statusCode: 500,
+            body: JSON.stringify(err)
+        });
+    }
+}
+
+/**
+ * Function to change password
+ * @param {*} currentPassword 
+ * @param {*} newPassword 
+ */
+async function changePassword(currentPassword, newPassword, accessToken, callback) {
+    const params = {
+        AccessToken: accessToken,
+        PreviousPassword: currentPassword,
+        ProposedPassword: newPassword
+    };
+
+    try {
+        await cognito.changePassword(params).promise();
+        callback(null, {
+            statusCode: 200,
+            body: JSON.stringify({ "message": "Password changed successfully" })
+        });
+        isFormDirty = false;
+    } catch (error) {
+        callback(null, {
+            statusCode: 500,
+            body: JSON.stringify(error)
+        });
     }
 }
 
@@ -196,6 +319,46 @@ exports.handler = (event, context, callback) => {
             }
 
             break;
+        case "POST /api/account/authenticate":
+            body = event.body;
+
+            if (typeof body === "string") body = JSON.parse(body);
+
+            checkRequiredKeys(body, ["username", "password"], callback);
+
+            calculateSecretHash(body.username)
+                .then(secretHash => {
+                    const params = {
+                        AuthFlow: 'USER_PASSWORD_AUTH',
+                        ClientId: '2lave0d420lofl9ead9h87mi41',  // Your App Client ID
+                        AuthParameters: {
+                            USERNAME: body.username,
+                            PASSWORD: body.password,
+                            SECRET_HASH: secretHash
+                        }
+                    };
+
+                    cognito.initiateAuth(params, (err, data) => {
+                        if (err) throw new Error("Error initiating auth:", err);
+                        
+                        if (data.ChallengeName === 'NEW_PASSWORD_REQUIRED') {
+                            handleNewPasswordChallenge(data.Session, body.username, body.password, callback);
+                        } else {
+                            callback(null, {
+                                statusCode: 200,
+                                body: JSON.stringify(data.AuthenticationResult),
+                            });
+                        }
+                    });
+                })
+                .catch(err => {
+                    callback(null, {
+                        statusCode: 500,
+                        body: JSON.stringify(err),
+                    });
+                });
+
+            break;
         case "POST /api/account/register":
             try {
                 body = event.body;
@@ -248,6 +411,41 @@ exports.handler = (event, context, callback) => {
             }
 
             break;
+        case "POST /api/account/forget-password":
+            try {
+                body = event.body;
+
+                if (typeof body === "string") body = JSON.parse(body);
+
+                checkRequiredKeys(body, ["username"], callback);
+
+                forgotPassword(body.username, callback);
+            } catch (err) {
+                callback(null, {
+                    statusCode: 500,
+                    body: JSON.stringify(err)
+                });
+            }
+
+            break;
+        case "POST /api/account/reset-password":
+            try {
+                body = event.body;
+
+                if (typeof body === "string") body = JSON.parse(body);
+
+                checkRequiredKeys(body, ["current_password", "new_password"], callback);
+                checkRequiredKeys(auth, ["authorization"], callback);
+
+                changePassword(body.current_password, body.new_password, auth.authorization, callback);
+            } catch (err) {
+                callback(null, {
+                    statusCode: 500,
+                    body: JSON.stringify(err)
+                });
+            }
+            
+            break; 
         default:
             if (!event.routeKey) {
                 callback(null, {
