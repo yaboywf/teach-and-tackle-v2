62de80fe4d5c595edda388fceeaf776ad03d6996
Dylan <dylanyeowf@gmail.com>
2025-07-26 15:21:42 +0800

    update

--- js/register2.js
+++ js/register2.js
@@ -2,9 +2,11 @@ if (!sessionStorage.getItem("admin_number")) window.location.href = "/pages/regi
 
 document.getElementById("signupForm").addEventListener("submit", (e) => {
     e.preventDefault();
-    const code = document.getElementById("code").value;
+
+    const code = Array.from(inputs).map(input => input.value).join('');
     if (code === "") return showMessage("Please enter the confirmation code");
-    
+    if (code.length !== 6) return showMessage("Please enter a valid confirmation code");
+
     axios.post("https://s5y8kqe8x9.execute-api.us-east-1.amazonaws.com/api/account/confirm-email", { username: sessionStorage.getItem("admin_number"), code })
         .then(() => {
             showMessage("Account confirmed", "success");
@@ -16,3 +18,38 @@ document.getElementById("signupForm").addEventListener("submit", (e) => {
             showMessage("Error confirming account");
         })
 })
+
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach((input, index) => {
+    input.addEventListener('input', (e) => {
+        e.target.value = e.target.value.replace(/\D/g, '');
+
+        if (e.target.value.length > 1) e.target.value = e.target.value.slice(0, 1);
+        if (index < inputs.length - 1 && e.target.value !== '') inputs[index + 1].focus();
+        if (Array.from(inputs).every(input => input.value !== '')) document.getElementById("signupForm").submit();
+    });
+
+    input.addEventListener('keydown', (e) => {
+        if (e.key === 'Backspace' && e.target.value === '') {
+            if (index > 0) inputs[index - 1].focus();
+        } else if (e.key === 'ArrowLeft' && index > 0) {
+            if (index > 0) inputs[index - 1].focus();
+        } else if (e.key === 'ArrowRight' && index < inputs.length - 1) {
+            if (index < inputs.length - 1) inputs[index + 1].focus();
+        }
+    });
+});
+
+inputs[0].addEventListener('paste', (e) => {
+    const paste = e.clipboardData.getData('Text');
+    const numericPaste = paste.replace(/\D/g, '');
+    inputs.forEach((input, index) => {
+        input.value = numericPaste[index] || '';
+        if (numericPaste[index] && index < inputs.length - 1) {
+            inputs[index + 1].focus();
+        }
+    });
+
+    e.preventDefault();
+});
